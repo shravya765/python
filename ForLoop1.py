f120a50a37efcc9b1561c18b27c2676744d81656
sandeep.vennam <vennam.sandeepreddy@gmail.com>
2018-11-14 18:12:08 +0530

    For Loop basic operations

---
+++ ForLoop1.py
@@ -0,0 +1,10 @@
+for letter in "Python coding":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+
+for friend in friends:
+    print(friend)
+    print(len(friends))
+    print(type(friends))
+    print(id(friends))
